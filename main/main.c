92df871f6e773b85086211a247dea05a13dd1ca1
Mert Can Ergün <mcergun@users.noreply.github.com>
2025-06-28 15:38:08 +0300

    Moved fan related fields under a struct

--- main/main.c
+++ main/main.c
@@ -17,26 +17,41 @@
 #define DHT22_GPIO          GPIO_NUM_10
 #define DHT22_SENSOR_TYPE   DHT_TYPE_AM2301
 
-#define FAN_PWM_FREQ    (25 * 1000)
-#define FAN_PWM_TIMER   LEDC_TIMER_0
-#define FAN_PWM_SPEED   LEDC_LOW_SPEED_MODE
-#define FAN_PWM_CHANNEL LEDC_CHANNEL_0
-#define FAN_GPIO        GPIO_NUM_8
+#define FAN_PWM_FREQ        (25 * 1000)
+#define FAN_PWM_TIMER       LEDC_TIMER_0
+#define FAN_PWM_SPEED       LEDC_LOW_SPEED_MODE
+#define FAN_PWM_CHANNEL     LEDC_CHANNEL_0
+#define FAN_PWM_DUTY_RES    LEDC_TIMER_10_BIT
+#define FAN_PWM_DUTY_MAX    ((0x1u << ((size_t)FAN_PWM_DUTY_RES)) - 1)
+#define FAN_GPIO            GPIO_NUM_8
 
 esp_err_t InitializePwmController(void);
 esp_err_t InitializeNvs(void);
 
-int16_t minHumidity = 30 * 10;
-int16_t maxHumidity = 70 * 10;
-int16_t steps = 0;
-uint32_t duty = 0;
-uint32_t maxDuty = 1024;
+typedef struct fan_control_state
+{
+    /// @brief minimum humidity value * 10 for fan to start working
+    int16_t humidty_min;
+    /// @brief maximum humidty value * 10 for max fan cycles
+    int16_t humidity_max;
+    /// @brief granularity of fan control steps
+    int16_t steps;
+    /// @brief current duty of fan
+    uint32_t duty;
+} fan_control_state_t;
+
+fan_control_state_t fan = {
+    .humidty_min = 30 * 10,
+    .humidity_max = 70 * 10,
+    .steps = (70 - 30) * 10,
+    .duty = 0,
+};
 
 esp_err_t InitializePwmController(void)
 {
     ledc_timer_config_t pwmTimer = {
         .speed_mode = FAN_PWM_SPEED,
-        .duty_resolution = LEDC_TIMER_10_BIT,
+        .duty_resolution = FAN_PWM_DUTY_RES,
         .timer_num = FAN_PWM_TIMER,
         .freq_hz = FAN_PWM_FREQ,
         .clk_cfg = LEDC_AUTO_CLK,
@@ -48,8 +63,8 @@ esp_err_t InitializePwmController(void)
         .channel = FAN_PWM_CHANNEL,
         .intr_type = LEDC_INTR_DISABLE,
         .timer_sel = FAN_PWM_TIMER,
-        .duty = duty,
-        .hpoint = duty,
+        .duty = fan.duty,
+        .hpoint = fan.duty,
         .sleep_mode = LEDC_SLEEP_MODE_NO_ALIVE_NO_PD,
     };
     esp_err_t ret;
@@ -93,25 +108,24 @@ void app_main(void)
         ESP_LOGI(LOG_TAG, "NVS initialized");
     }
 
-    steps = maxHumidity - minHumidity;
     while(1)
     {
         ret = dht_read_data(DHT22_SENSOR_TYPE, DHT22_GPIO, &humidity, &temperature);
         
-        if (humidity < minHumidity)
+        if (humidity < fan.humidty_min)
         {
-            duty = 0;
+            fan.duty = 0;
         }
-        else if (humidity > maxHumidity)
+        else if (humidity > fan.humidity_max)
         {
-            duty = maxDuty;
+            fan.duty = FAN_PWM_DUTY_MAX;
         }
         else
         {
-            duty = ((humidity - minHumidity) * maxDuty) / steps;
+            fan.duty = ((humidity - fan.humidty_min) * FAN_PWM_DUTY_MAX) / fan.steps;
         }
 
-        ESP_ERROR_CHECK(ledc_set_duty(FAN_PWM_SPEED, FAN_PWM_CHANNEL, duty));
+        ESP_ERROR_CHECK(ledc_set_duty(FAN_PWM_SPEED, FAN_PWM_CHANNEL, fan.duty));
         ESP_ERROR_CHECK(ledc_update_duty(FAN_PWM_SPEED, FAN_PWM_CHANNEL));
         if (ret == ESP_OK)
         {
