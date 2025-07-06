95bdf09693beba47d5731c389a92c3c6c8af347e
Mert Can Ergün <mcergun@users.noreply.github.com>
2025-07-06 17:41:52 +0300

    Added log message when NVS is reset

--- main/main.c
+++ main/main.c
@@ -27,6 +27,7 @@ esp_err_t init_nvs(void)
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
         ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
         ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_LOGI(LOG_TAG, "NVS cleared");
         ret = nvs_flash_init();
     }
 
